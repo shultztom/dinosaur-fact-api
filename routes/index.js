2b09dc0583e59ca8ee5212f4edf28622cf486f53
shultztom <shultzthomask@sau.edu>
2018-09-05 20:08:46 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var router = express.Router();
+const fs = require("fs");
+const dinoContent = fs.readFileSync("dinosaurs.json");
+const jsonContent = JSON.parse(dinoContent);
+
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Dinosaur API" });
+});
+
+router.get("/dinosaurs", function(req, res, next) {
+  res.json(jsonContent);
+});
+
+router.get("/dinosaurs/random", function(req, res, next) {
+  let randomIndex = getRandomInt(jsonContent.length);
+  res.json(jsonContent[randomIndex]);
+});
+
+router.get("/dinosaurs/random/name", function(req, res, next) {
+  let randomIndex = getRandomInt(jsonContent.length);
+  let json = { Name: jsonContent[randomIndex].Name };
+  res.json(json);
+});
+
+router.get("/dinosaurs/random/description", function(req, res, next) {
+  let randomIndex = getRandomInt(jsonContent.length);
+  let json = { Description: jsonContent[randomIndex].Description };
+  res.json(json);
+});
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+module.exports = router;

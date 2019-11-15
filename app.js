bba7a51d0e38151fff58d4db0ba3cb0927f09197
shultztom <shultzthomask@sau.edu>
2019-11-15 17:53:41 -0600

    cleaned up and added logger

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
@@ -17,10 +16,11 @@ app.use(cors());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-//app.use(logger("dev"));
+app.use(
+  logger(":method :url :status :res[content-length] - :response-time ms")
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);

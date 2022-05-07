01a2d638c6dc0d062bc348178a231f862a06253c
shultztom <shultzthomask@sau.edu>
2022-05-07 09:33:31 -0500

    moved to gcp registry

--- helmDeploy.sh
+++ helmDeploy.sh
@@ -1,1 +1,4 @@
-helm upgrade --install dino-facts-api shultzlab/http-deploy  -f .helm/deploy/values.yaml
+gcloud auth print-access-token | helm registry login -u oauth2accesstoken \
+--password-stdin https://us-central1-docker.pkg.dev
+
+helm upgrade --install dino-facts-api oci://us-central1-docker.pkg.dev/shultzlab/shultzlab-helm/http-deploy --version 0.1.0 -f .helm/deploy/values.yaml
--- nexus-deply.sh
+++ nexus-deply.sh
@@ -1,9 +1,9 @@
 PACKAGE_NAME=$(node -p "require('./package.json').name")
 PACKAGE_VERSION=$(node -p "require('./package.json').version")
 # Build tag with version
-docker build -t shultzlab.jfrog.io/default-docker-virtual/shultztom/$PACKAGE_NAME:$PACKAGE_VERSION .
-#docker push shultzlab.jfrog.io/default-docker-virtual/shultztom/$PACKAGE_NAME:$PACKAGE_VERSION
+docker build -t us-central1-docker.pkg.dev/shultzlab/docker-gcp/$PACKAGE_NAME:$PACKAGE_VERSION .
+docker push us-central1-docker.pkg.dev/shultzlab/docker-gcp/$PACKAGE_NAME:$PACKAGE_VERSION
 # Tag with latest
-docker tag shultzlab.jfrog.io/default-docker-virtual/shultztom/$PACKAGE_NAME:$PACKAGE_VERSION shultzlab.jfrog.io/default-docker-virtual/shultztom/$PACKAGE_NAME:latest
-docker push shultzlab.jfrog.io/default-docker-virtual/shultztom/$PACKAGE_NAME:latest
+docker tag us-central1-docker.pkg.dev/shultzlab/docker-gcp/$PACKAGE_NAME:$PACKAGE_VERSION us-central1-docker.pkg.dev/shultzlab/docker-gcp/$PACKAGE_NAME:latest
+docker push us-central1-docker.pkg.dev/shultzlab/docker-gcp/$PACKAGE_NAME:latest
 echo 'Done pushing image'

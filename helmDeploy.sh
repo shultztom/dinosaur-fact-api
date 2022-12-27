217455337d3922997a84ac0f5de040710583ae21
shultztom <shultzthomask@sau.edu>
2022-12-27 11:59:25 -0600

    moved k8 cluster

--- helmDeploy.sh
+++ helmDeploy.sh
@@ -1,4 +1,4 @@
 gcloud auth print-access-token | helm registry login -u oauth2accesstoken \
 --password-stdin https://us-central1-docker.pkg.dev
 
-helm upgrade --install dino-facts-api oci://us-central1-docker.pkg.dev/shultzlab/shultzlab-helm/http-deploy --version 0.1.0 -f .helm/deploy/values.yaml
+helm upgrade --install dino-facts-api oci://us-central1-docker.pkg.dev/shultzlab/shultzlab-helm/http-deploy --version 0.2.2 -f .helm/deploy/values.yaml

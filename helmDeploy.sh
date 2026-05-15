5b7adca5abd2bef3efd23a83a973c91983eacb23
shultztom <shultzthomask@sau.edu>
2026-05-14 20:36:38 -0500

    small script fix

--- helmDeploy.sh
+++ helmDeploy.sh
@@ -4,4 +4,7 @@ PACKAGE_VERSION=$(node -p "require('./package.json').version")
 gcloud auth print-access-token | helm registry login -u oauth2accesstoken \
 --password-stdin https://us-central1-docker.pkg.dev
 
-helm upgrade --install dino-facts-api oci://us-central1-docker.pkg.dev/shultzlab/shultzlab-helm/http-deploy --version 0.2.2 -f .helm/deploy/values.yaml --set image.tag "$PACKAGE_VERSION"
+helm upgrade --install dino-facts-api oci://us-central1-docker.pkg.dev/shultzlab/shultzlab-helm/http-deploy \
+  --version 0.2.2 \
+  -f .helm/deploy/values.yaml \
+  --set image.tag="$PACKAGE_VERSION"
